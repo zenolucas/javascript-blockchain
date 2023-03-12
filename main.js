b15f25b597782f3c4c5ea1811fb5c6d08e42155c
Zen Lucas <2200501@slu.edu.ph>
2023-03-12 21:53:50 +0800

    create Block class, and Blockchain class, along with their methods.

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
+const SHA256 = require('crypto-js/sha256')
+
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash(){
+        return SHA256(this.index + this.previousHash + JSON.stringify(this.data)).toString();
+    }
+}
 
+class Blockchain{
+    constructor(){
+        this.chain = [];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "03/12/2023", "Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    // disclaimer: in reality you can't add a block so easily, this is just for demonstration purposes.
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
     }
 }
